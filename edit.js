41c94fd895c6831a0bb02a739a1bc033062f7589
trojan666 <troyan666-92@mail.ru>
2014-04-12 17:22:56 +0400

    Minor refactoring + can draw lines now!

--- edit.js
+++ edit.js
@@ -1,35 +1,55 @@
 var CanvasManager = {
-    clickX: 0,
-    clickY: 0,
-    shapeDrawer: null,
 
-    onMouseDown: function(x, y) {
-        this.clickX = x - this.shapeDrawer.offsetLeft;
-        this.clickY = y - this.shapeDrawer.offsetTop;
-        this.shapeDrawer.start();
+    initialize: function() {
+        this.drawingCanvas = $('#drawing_canvas')[0];
+        this.imageCanvas = $('#screenshot_canvas')[0];
+
+        this.drawContext = this.drawingCanvas.getContext('2d');
+        this.mainContext = this.imageCanvas.getContext('2d');
+
+        this.offsetLeft= this.drawingCanvas.offsetLeft;
+        this.offsetTop = this.drawingCanvas.offsetTop;
+
+        this.drawingCanvas.addEventListener('mousedown', function(e) {CanvasManager.onMouseDown(e);});
+        this.drawingCanvas.addEventListener('mousemove', function(e) {CanvasManager.onMove(e);});
+        this.drawingCanvas.addEventListener('mouseup', function(e) {CanvasManager.onMouseUp(e);});
     },
 
-    onMove: function(x, y) {
-        if (this.shapeDrawer != null)
-            this.shapeDrawer.draw(this.clickX, this.clickY,
-                                  x - this.shapeDrawer.offsetLeft, y - this.shapeDrawer.offsetTop);
+    clickX: 0,
+    clickY: 0,
+
+    onMouseDown: function(event) {
+        if (this.shapeDrawer != null) {
+            var x = event.clientX, y = event.clientY;
+
+            this.clickX = x - this.offsetLeft;
+            this.clickY = y - this.offsetTop;
+
+            this.shapeDrawer.start();
+
+            this.drawContext.lineWidth = 4;
+        }
+    },
+
+    onMove: function(event) {
+        var x = event.clientX, y = event.clientY;
+
+        if (this.shapeDrawer != null) {
+            this.drawContext.clearRect(0, 0, this.drawingCanvas.width, this.drawingCanvas.height);
+            this.shapeDrawer.draw(this.drawContext, this.clickX, this.clickY, x - this.offsetLeft, y - this.offsetTop);
+        }
     },
 
     onMouseUp: function() {
-        this.shapeDrawer.stop();
+        if (this.shapeDrawer != null) {
+            this.shapeDrawer.stop();
+            this.mainContext.drawImage(this.drawingCanvas, 0, 0);
+        }
     }
 };
 
-function AbstractDrawer (drawingCanvas, imageCanvas) {
-    this.drawingCanvas = drawingCanvas;
-    this.imageCanvas = imageCanvas;
-    this.ctx = this.drawingCanvas.getContext('2d');
+function AbstractDrawer () {
     this.isDrawing = false;
-
-    this.offsetLeft = this.drawingCanvas.offsetLeft;
-    this.offsetTop = this.drawingCanvas.offsetTop;
-
-    this.ctx.lineWidth = 4;
 }
 
 AbstractDrawer.prototype.start = function() {
@@ -38,67 +58,68 @@ AbstractDrawer.prototype.start = function() {
 
 AbstractDrawer.prototype.stop = function() {
     this.isDrawing = false;
-    this.imageCanvas.getContext('2d').drawImage(this.drawingCanvas, 0, 0);
 };
 
 /* Rectangle drawer */
-function RectangleDrawer(drawingCanvas, imageCanvas) {
-    AbstractDrawer.call(this, drawingCanvas, imageCanvas);
+function RectangleDrawer() {
+    AbstractDrawer.call(this);
 }
 
 RectangleDrawer.prototype = Object.create(AbstractDrawer.prototype);
 
-RectangleDrawer.prototype.draw = function(x1, y1, x2, y2) {
+RectangleDrawer.prototype.draw = function(ctx, x1, y1, x2, y2) {
     if (this.isDrawing) {
-        this.ctx.clearRect(0, 0, this.drawingCanvas.width, this.drawingCanvas.height);
-        this.ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
+        ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
     }
 };
 
 /* Circle drawer */
-function CircleDrawer(drawingCanvas, imageCanvas) {
-    AbstractDrawer.call(this, drawingCanvas, imageCanvas);
+function CircleDrawer() {
+    AbstractDrawer.call(this);
 }
 
 CircleDrawer.prototype = Object.create(AbstractDrawer.prototype);
 
-CircleDrawer.prototype.draw = function(x1, y1, x2, y2) {
+CircleDrawer.prototype.draw = function(ctx, x1, y1, x2, y2) {
     if (this.isDrawing) {
-      var ctx = this.ctx;
-      ctx.clearRect(0, 0, this.drawingCanvas.width, this.drawingCanvas.height);
       ellipse(ctx, x1, y1, x2 - x1, y2 - y1);
   }
 };
 
+function LineDrawer() {
+    AbstractDrawer.call(this);
+}
+
+LineDrawer.prototype = Object.create(AbstractDrawer.prototype);
+
+LineDrawer.prototype.draw = function(ctx, x1, y1, x2, y2) {
+    if (this.isDrawing) {
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.closePath();
+        ctx.stroke();
+    }
+};
 
 window.onload = function() {
-    function getDrawer(id, drawCanvas, imageCanvas) {
+
+    function getDrawer(id) {
         switch (id) {
-            case 'rectangle': return new RectangleDrawer(drawCanvas, imageCanvas); break;
-            case 'ellipse': return new CircleDrawer(drawCanvas, imageCanvas); break;
-            default: return new RectangleDrawer(drawCanvas, imageCanvas); break;
+            case 'rectangle': return new RectangleDrawer();
+            case 'ellipse': return new CircleDrawer();
+            case 'line': return new LineDrawer();
+            default: return new RectangleDrawer();
         }
         return null;
     }
 
     $('.draw_method').click(function() {
-        var drawingCanvas = $('#drawing_canvas');
-        var imageCanvas = $('#screenshot_canvas');
-
-        CanvasManager.shapeDrawer = getDrawer(this.id, drawingCanvas[0], imageCanvas[0]);
-
-        drawingCanvas.mousedown(function (e) {
-            CanvasManager.onMouseDown(e.clientX, e.clientY);
-        });
-
-        drawingCanvas.mousemove(function(e) {
-            CanvasManager.onMove(e.clientX, e.clientY);
-        });
-
-        drawingCanvas.mouseup(function(e) {
-            CanvasManager.onMouseUp();
-        });
+        console.log(this.id);
+        CanvasManager.shapeDrawer = getDrawer(this.id);
     });
+
+    CanvasManager.initialize();
 };
 
 function ellipse(drawCtx, aX, aY, aWidth, aHeight) {
@@ -108,7 +129,7 @@ function ellipse(drawCtx, aX, aY, aWidth, aHeight) {
         eY = aY + aHeight,
         mX = aX + aWidth / 2,
         mY = aY + aHeight / 2;
-    
+
     drawCtx.beginPath();
     drawCtx.moveTo(aX, mY);
     drawCtx.bezierCurveTo(aX, mY - vB, mX - hB, aY, mX, aY);
