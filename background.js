7bd089a9bc6979666a7b1a0e0abd0d6f2fbacb73
trojan666 <troyan666-92@mail.ru>
2014-05-14 21:58:15 +0400

    Requesting new token if locasStorage contains an expired one

--- background.js
+++ background.js
@@ -5,33 +5,33 @@ var yandexLoginUrl = 'https://oauth.yandex.ru/authorize?response_type=token&clie
 
 var tabId = 0;
 
-function onTabUpdate() {
-    if (!localStorage.accessToken) {
-        chrome.tabs.query({active: true}, function(tabs) {
-            for (var i = 0; i < tabs.length; ++i) {
-                if (tabs[i].url.indexOf(successfulLoginUrl) == 0) {
-                    var tab = tabs[i];
-                    var token = extractTokenFromUrl(tab.url);
-                    localStorage.accessToken = token;
-                    chrome.tabs.onUpdated.removeListener(onTabUpdate);
-                    chrome.tabs.remove(tab.id);
+function getToken() {
+    chrome.tabs.query({active: true}, function(tabs) {
+        for (var i = 0; i < tabs.length; ++i) {
+            console.log("Trying: " + i);
+            if (tabs[i].url.indexOf(successfulLoginUrl) == 0) {
+                var tab = tabs[i];
+                var token = extractTokenFromUrl(tab.url);
 
-                    console.log('Token: ' + token);
-                    console.log(tabs.length);
-                }
+                localStorage.accessToken = token.token;
+
+                var date = new Date();
+                date.setSeconds(date.getSeconds() + token.expires);
+                localStorage.expires = date.getTime();
+
+                chrome.tabs.remove(tab.id);
+                chrome.tabs.onUpdated.removeListener(getToken);
             }
-        });
-    }
+        }
+    });
 }
 
 window.onload = function () {
-    chrome.tabs.onUpdated.addListener(onTabUpdate);
 
     var onClicked = function() {
+        if (!localStorage.accessToken || parseInt(localStorage.expires) < new Date().getTime()) {
 
-        if (!localStorage.accessToken) {
-            console.log("No access token!");
-
+            chrome.tabs.onUpdated.addListener(getToken);
             chrome.tabs.create({}, function(tab) {
                 chrome.tabs.update(tab.id, {url : yandexLoginUrl});
             });
@@ -48,8 +48,11 @@ window.onload = function () {
 
 function extractTokenFromUrl(url) {
     var params = url.split('#')[1];
-    var tokenParam = params.split('&')[0];
-    return tokenParam.split('=')[1];
+    params = params.split('&');
+    var tokenParam = params[0];
+    var expiresParam = params[3];
+
+    return { token : tokenParam.split('=')[1], expires : parseInt(expiresParam.split('=')[1]) };
 }
 
 function takeScreenshot() {
